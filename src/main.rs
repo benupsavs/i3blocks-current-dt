f7ceef79f57041c132afdf1a5fbb22065a6a7af3
Ben Upsavs <ben@upsavs.com>
2023-03-04 03:12:17 -0800

    Remove space padding from day

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use chrono::{Local, Timelike};
 fn main() {
     loop {
         let now = Local::now();
-        println!("{}", now.format("%a %b %e %H:%M:%S"));
+        println!("{}", now.format("%a %b %-e %H:%M:%S"));
         _ = std::io::stdout().flush();
         let sleep_nanos = 1000000000 - now.nanosecond();
         thread::sleep(Duration::new(0, sleep_nanos));
